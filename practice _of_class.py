b1136987144b168349f362c8a1ddc677db74d144
Manas Bisht <bishtmanas2008@gmail.com>
2025-05-08 19:02:10 +0530

    deleted practice_of_class.py

--- practice _of_class.py
+++
@@ -1,4 +0,0 @@
-def cars (name , model , price ):
-    print (f"{name} is the best car .\nmodel = {model}\nprice = {price}")
-cars ("\"Merceders\"", "after purchasing you will know that .", "120000$")
-        
