4cc173e7e16a67176ef355306a6c607ded148cad
Manas Bisht <bishtmanas2008@gmail.com>
2025-05-09 13:28:31 +0530

    Refactored to-do list app: now you can remove tasks by entering a task number , improved input validation, and better error handling

--- simple_to_do_list.py
+++ simple_to_do_list.py
@@ -1,4 +1,34 @@
-def melist():
+def addtask(my_list):
+    task = input("🆕 Enter your new task: ").strip().title()
+    if task:
+        my_list.append(task)
+        print(f"✅ Task added: {task}")
+    else:
+        print("⚠️ Task can't be empty!")
+        
+def viewtask(my_list):
+    if not my_list:
+        print ("yours task is empty !")
+                
+    else:
+        print ('\nyour task->')
+        for index , task in enumerate(my_list,1):
+            print (f"{index}. {task}")
+            
+def removetask(my_list):
+    if not my_list:
+        print("❌ ,Your task list is already empty!")
+        return
+    viewtask(my_list)
+    try:
+        rem = int(input("🔢 Enter the number of the task to remove: "))
+        if 1<=rem<=len(my_list):
+            removed = my_list.pop(rem - 1 )
+            print(f"🗑️ Removed: {removed}")
+    except ValueError:
+        print("❌ Please enter a valid number.")        
+
+def to_do_task_app():
     my_list = []
     
     while True:
@@ -8,38 +38,19 @@ def melist():
         print("3. ❌ Remove Task")
         print("4. 🚪 Exit")
 
-        you_task = input("What do you want to do (1-4)? ")
+        choice = input("👉 What do you want to do (1-4)? : ").strip()
 
-        if you_task == "1":
-            add = input("What is your task? ")
-            my_list.append(add)
-            print(f"✅ Task added: {add}")
-            
-        elif you_task == "2":
-            
-            if not my_list:
-                print ("you task is empty !")
-                
-            else:
-                print ('\nyour task ')
-                index = 1
-                for task in my_list:
-                    print (f"{index}.",task )
-                    index +=1
-                    
-        elif you_task == "3":
-            
-            if not my_list:
-                print("❌ Your task list is already empty!")
-                
-            else :
-                rem = input ("what you want to remove ?? : ").lower()
-                my_list.remove(rem)
-                print (f"removed {rem} from your list ")
-                
-        elif you_task == "4":
-            print ("Thankyou bye bye sir/madam")
+        if choice == "1":
+            addtask(my_list)
+        elif choice == "2":
+            viewtask(my_list)
+        elif choice == "3":
+            removetask(my_list)
+        elif choice == "4":
+            print("👋 Thank you for using the To-Do List. Bye!")
             break
-        else :
-            print("invalid input ! please try again")
-melist()
+        else:
+            print("⚠️ Invalid input! Please choose between 1 and 4.")
+
+to_do_task_app()
+        
