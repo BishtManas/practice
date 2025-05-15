fe3812aaf6fd19658b195c9ce1d1914a6f9a832d
Manas Bisht <bishtmanas2008@gmail.com>
2025-05-15 17:48:59 +0530

    question1

---
+++ class and object question(easy )/easy question /ques1.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self ,name , age ):
+        self.name = name 
+        self.age = age
+           
+p1 = Person("manas", 24) # its attributes 
+print (p1.name)
+print (p1.age)
